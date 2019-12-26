d1a93c348a0b7e06a9182e388c2ed06bc731e911
Matt Del Signore <mattdelsig@me.com>
2019-12-25 21:27:46 -0500

    Broke the file into seperate folders to prevent collisions with the main function.

--- day1/day1.go
+++ day1/day1.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	f, err := os.Open("files/day1.txt")
+	f, err := os.Open("day1.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -19,7 +19,7 @@ func main() {
 	scanner := bufio.NewScanner(reader)
 
 	var totalMass float64 = 0.0
-	// Read through the file, print out the output
+	// read through the file, print out the output
 	for scanner.Scan() {
 		mass, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
@@ -27,10 +27,18 @@ func main() {
 		}
 		totalMass += calculateFuel(mass)
 	}
-	fmt.Printf("%f", totalMass)
+
+	fmt.Printf("%d\n", int(totalMass))
 }
 
 /** Calculate the fuel needed for a given mass */
 func calculateFuel(mass float64) float64 {
-	return math.Floor(mass/3.0) - 2.0
+	fuel := math.Floor(mass/3.0) - 2.0
+	// Base case, negative mass
+	if fuel <= 0 {
+		return 0
+	}
+
+	// Calculate the mass needed for the fuel's fuel as well
+	return fuel + calculateFuel(fuel)
 }
---
+++ day2/day2.go
@@ -0,0 +1,11 @@
+// Advent of Code Day 2
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("files/day2.txt")
+}
